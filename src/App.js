a6cf817cb2ad7751efd5bcd77bd46b4d886585cd
kourin <kourin1996@gmail.com>
2018-08-23 13:57:02 -0700

    merge

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-<<<<<<< HEAD
-=======
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
@@ -14,17 +12,11 @@ import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import TextField from '@material-ui/core/TextField'
 
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
 //import IconButton from '@material-ui/core/IconButton'
 //import MenuIcon from '@material-ui/icons/Menu'
 import './App.css'
 
-<<<<<<< HEAD
-const API_URI = 'http://localhost:8080'
-//const API_URI = 'http://internship-jp1.interlink.or.jp/getbalance/leign'
-=======
 const API_URI = 'http://internship-jp1.interlink.or.jp/api'
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
 
 const styles = {
   root: {
@@ -49,31 +41,13 @@ class App extends React.Component {
     this.state = {
       balance: 200,
       immatureBalance: 50,
-<<<<<<< HEAD
-=======
       loginOpen: false,
       sendOpen: false
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
     }
   }
 
   async fetchBlance(e)
   {
-<<<<<<< HEAD
-    //try
-    //{
-    //  const j = await fetch(`${API_URI}/getPeers`).then(x => x.json())
-    //  this.setState({balance: j.balance, immatureBalance: j.immatureBalance})
-    //}
-    //catch(e)
-    //{
-    //  console.log(e)
-    //}
-
-    try
-    {
-      const j = await fetch('http://internship-jp1.interlink.or.jp/getbalance/itleigns').then(x => x.json())
-=======
     try
     {
       localStorage.setItem('id', 'itleigns')
@@ -96,7 +70,6 @@ class App extends React.Component {
       const id = localStorage.getItem('id')
       console.log(id)
       const j = await fetch(`${API_URI}/sendmoney/${id}/${this.state.key}/${amount}/${target}`).then(x => x.json())
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
       this.setState({balance: j.balance})
     }
     catch(e)
@@ -105,8 +78,6 @@ class App extends React.Component {
     }
   }
 
-<<<<<<< HEAD
-=======
   loginOpen = () =>
   {
     this.setState({ loginOpen: true })
@@ -129,7 +100,6 @@ class App extends React.Component {
     this.setState({ sendOpen: false })
   }
 
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
   render()
   {
     const {classes} = this.props
@@ -140,20 +110,12 @@ class App extends React.Component {
             <Typography variant='title' color='inherit' className={classes.flex}>
                 SovoloCoin
             </Typography>
-<<<<<<< HEAD
-            <Button color='inherit'>Login</Button>
-=======
             <Button color='inherit' onClick={this.loginOpen}>Login</Button>
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
           </Toolbar>
         </AppBar>
         <div className='container'>
           <img src={process.env.PUBLIC_URL + '/logo.png'} alt='logo' className='icon' />
           <Typography variant='title' color='inherit' className='balance'>BALANCE {this.state.balance} SVL</Typography>
-<<<<<<< HEAD
-          <Button color='secondary' onClick={()=>this.fetchBlance()} className='send' >Send</Button>
-        </div>
-=======
           <Button color='secondary' onClick={()=>{this.fetchBlance();this.sendOpen();}} className='send' >Send</Button>
         </div>
 
@@ -224,7 +186,6 @@ class App extends React.Component {
             </Button>
           </DialogActions>
         </Dialog>
->>>>>>> 8bcde20a53882903ca325cc8930645c2cd81841b
       </div>
     )
   }
